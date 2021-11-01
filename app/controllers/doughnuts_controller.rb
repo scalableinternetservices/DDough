aba6d16f3ada765d915c68598c2172379382253a
Iris Moini <azadeh@ucsb.edu>
2021-10-31 20:32:22 -0700

    implemented post /doughnuts and created form

--- app/controllers/doughnuts_controller.rb
+++ app/controllers/doughnuts_controller.rb
@@ -6,5 +6,24 @@ class DoughnutsController < ApplicationController
   def show
     @doughnut = Doughnut.find(params[:id])
   end
+
+  def new
+    @doughnut = Doughnut.new
+  end
+
+  def create
+    @doughnut = Doughnut.new(doughnut_params)
+
+    if @doughnut.save
+      redirect_to @doughnut
+    else
+      render :new
+    end
+  end
+
+  private
+    def doughnut_params
+      params.require(:doughnut).permit(:name, :price, :description)
+    end
   
 end
--- app/models/doughnut.rb
+++ app/models/doughnut.rb
@@ -1,2 +1,5 @@
 class Doughnut < ApplicationRecord
+    validates :name, presence: true
+    validates :price, presence: true
+    validates :description, presence: true
 end
