d70dd8b126a31d01fe761051974c5eba27459ac4
Alan Roddick <aroddick@ucsb.edu>
2021-11-03 20:30:33 -0700

    Return different status code based on username match

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -2,6 +2,10 @@ class RegisterController < ApplicationController
   skip_before_action :verify_authenticity_token, :authorized, only: [:create]
 
   def create
+    if User.exists?(username: user_params[:username])
+      render json: { error: 'Username taken'}, status: 409
+      return
+    end
     @user = User.create(user_params)
     if @user.valid?
       @token = encode_token({ user_id: @user.id })
