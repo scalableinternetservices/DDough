9fcf2f25abb123e58a3aba6fe265382d9b788a8e
Tina Shi <tshi@ucsb.edu>
2021-11-09 17:05:02 -0800

    styled item cards more to look better

--- app/javascript/components/ItemCard.jsx
+++ app/javascript/components/ItemCard.jsx
@@ -1,26 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Placeholder1 from "images/item_placeholder_1.JPG";
 import Placeholder2 from "images/item_placeholder_2.JPG";
 
-export default(props) => (
-	<div className="item-card-container">
-		<img 
-			src={props.img || (props.idx % 2 == 0 ? Placeholder1 : Placeholder2)}
-			alt={`Image of ${props.name}`} 
-			className="item-card-img"
-		/>
-		<p className="item-card-name">{props.name}</p>
-		<p className="item-card-price">{props.price}</p>
+export default(props) => {
+	const [displayBuy, setDisplayBuy] = useState(false);
 
-		{props.description && (
-			<p className="item-card-description">{props.description}</p>
-		)}
-		{props.quantity && (
-			<p className="item-card-quantity">{props.quantity}</p>
-		)}
-		<form>
-			<input type="number" defaultValue={1} />
-			<input type="submit" value="Buy Now" />
-		</form>
-	</div>
-);
+	return (
+		<div 
+			className="item-card-container" 
+			onMouseEnter={() => setDisplayBuy(true)}
+			onMouseLeave={() => setDisplayBuy(false)}
+		>
+
+			{displayBuy && (
+				<form className="purchase-form">
+					<input type="number" defaultValue={1} className="purchase-quantity" />
+					<input type="submit" value="Buy Now" className="buy-now-button" />
+				</form>
+			)}
+
+			<img 
+				src={props.img || (props.idx % 2 == 0 ? Placeholder1 : Placeholder2)}
+				alt={`Image of ${props.name}`} 
+				className="item-card-img"
+			/>
+			<p className="item-card-name">{props.name}</p>
+			<p className="item-card-price">{props.price}</p>
+
+			{props.description && (
+				<p className="item-card-description">{props.description}</p>
+			)}
+			{props.quantity && (
+				<p className="item-card-quantity">{props.quantity}</p>
+			)}
+		</div>
+	);
+}
