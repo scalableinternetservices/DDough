14a7b69101bcdb393a40dd8a355b9849089cbbc6
Christopher Yang <chrisyang365@gmail.com>
2021-11-11 20:35:23 -0800

    put foreign key ids in corresponding nested attribute objects

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -3,13 +3,13 @@ class Api::OrdersController < ApplicationController
 
   def index
     orders = Order.all
-    render json: orders, include: [:user => {:only => :username}, :doughnut => {:only => [:name, :price, :description]}]
+    render json: orders, include: [:user => {:only => [:id, :username]}, :doughnut => {:only => [:id, :name, :price, :description]}], only: [:id, :created_at, :updated_at, :user, :doughnut]
   end
 
   def create
     order = Order.create(order_params)
     if order.id
-      render json: order, include: [:user => {:only => :username}, :doughnut => {:only => [:name, :price, :description]}]
+      render json: order, include: [:user => {:only => [:id, :username]}, :doughnut => {:only => [:id, :name, :price, :description]}], only: [:id, :created_at, :updated_at, :user, :doughnut]
     else
       render json: { error: order.errors }
     end
@@ -18,7 +18,7 @@ class Api::OrdersController < ApplicationController
   def show
     @order ||= Order.find_by_id(params[:id])
     if @order
-      render json: @order, include: [:user => {:only => :username}, :doughnut => {:only => [:name, :price, :description]}]
+      render json: @order, include: [:user => {:only => [:id, :username]}, :doughnut => {:only => [:id, :name, :price, :description]}], only: [:id, :created_at, :updated_at, :user, :doughnut]
     else
       render json: { error: 'Order does not exist'}, status: 404
     end
