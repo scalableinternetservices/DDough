06a7bec34df495bffd01ac6b8680188f00e9aca2
Alan Roddick <aroddick@ucsb.edu>
2021-11-29 20:48:32 -0800

    Fix for ordering doughnut that doesn't exist

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,11 +1,13 @@
 class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :doughnut
+  validates :doughnut, presence: true
   validates :quantity, presence: true
   validate :quantity_smaller_than_stock
 
   private
   def quantity_smaller_than_stock
+    return if (doughnut == nil)
     doughnut.with_lock do
       if quantity > doughnut.quantity
         errors.add(:quantity, "Order item quantity cannot be greater than item stock")
