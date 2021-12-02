3bd779da5f89b7ddeae4479e989d767a058f485c
Christopher Yang <chrisyang365@gmail.com>
2021-12-01 16:29:35 -0800

    reduce number of orders

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-500.times do |i|
+100.times do |i|
     User.create(username: "buyer#{i}", password: "pw#{i}", role: "buyer")
     User.create(username: "seller#{i}", password: "pw#{i}", role: "seller")
     Doughnut.create(name: "donut#{i}", price: 10.99, description: "description#{i}", quantity: 100000, image_url: "https://url#{i}")
 end
 
 order_items = []
-500.times do |i|
+100.times do |i|
     order_items.append({ doughnut_id: i+1, quantity: 1})
 end
 Order.create(user_id: 1, order_items_attributes: order_items)
