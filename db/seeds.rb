1afa61e11de07ceee72d3382459544a7a932cf94
Alan Roddick <aroddick@ucsb.edu>
2021-12-02 15:24:35 -0800

    update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 10.times do |i|
-    User.create(username: "buyer#{i}", password: "pw#{i}", role: "buyer")
-    User.create(username: "seller#{i}", password: "pw#{i}", role: "seller")
-    Doughnut.create(name: "donut#{i}", price: 10.99, description: "description#{i}", quantity: 100000, image_url: "https://url#{i}")
+  User.create(username: "seller#{i}", password: "pw", role: "seller")
+end
+
+100.times do |i|
+  Doughnut.create(name: "donut#{i}", price: 10.99, description: "description#{i}", quantity: 100000, image_url: "https://url#{i}")
+end
+
+100.times do |i|
+  user = User.create(username: "buyer#{i}", password: "pw", role: "buyer")
+  100.times do |j|
+    Order.create(user: user, order_items_attributes: [
+      { doughnut_id: j, quantity: 10 }
+    ])
+  end
 end
