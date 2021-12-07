dc00b7e7e320c79302707efd1735c5396ff40e86
Alan Roddick <aroddick@ucsb.edu>
2021-12-07 00:10:39 -0800

    Remove seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,17 +10,17 @@
 end
 
 10000.times do |i|
-  Doughnut.create(name: "donut#{i}", price: 10.99, description: "description#{i}", quantity: 100000, image_url: "https://url#{i}")
+  Doughnut.create(name: "donut#{i}", price: 10.99, description: "description#{i}", quantity: 1000000, image_url: "https://url#{i}")
 end
 
 10000.times do |i|
   user = User.create(username: "buyer#{i}", password: "pw", role: "buyer")
-  100.times do |j|
+  10.times do |j|
     Order.create(user: user, order_items_attributes: [
-      { doughnut_id: j, quantity: 10 },
-      { doughnut_id: j+1, quantity: 10 },
-      { doughnut_id: j+2, quantity: 10 },
-      { doughnut_id: j+3, quantity: 10 },
+      { doughnut_id: j, quantity: 1 },
+      { doughnut_id: j+1, quantity: 1 },
+      { doughnut_id: j+2, quantity: 1 },
+      { doughnut_id: j+3, quantity: 1 },
     ])
   end
 end
