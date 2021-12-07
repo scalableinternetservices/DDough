fa3db565a998b24d279456771c02da3d3258102c
Alan Roddick <aroddick@ucsb.edu>
2021-12-06 22:32:36 -0800

    More seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,15 +9,18 @@
   User.create(username: "seller#{i}", password: "pw", role: "seller")
 end
 
-100.times do |i|
+10000.times do |i|
   Doughnut.create(name: "donut#{i}", price: 10.99, description: "description#{i}", quantity: 100000, image_url: "https://url#{i}")
 end
 
-100.times do |i|
+10000.times do |i|
   user = User.create(username: "buyer#{i}", password: "pw", role: "buyer")
   100.times do |j|
     Order.create(user: user, order_items_attributes: [
-      { doughnut_id: j, quantity: 10 }
+      { doughnut_id: j, quantity: 10 },
+      { doughnut_id: j+1, quantity: 10 },
+      { doughnut_id: j+2, quantity: 10 },
+      { doughnut_id: j+3, quantity: 10 },
     ])
   end
 end
